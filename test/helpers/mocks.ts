2618af8dda7f99de72ebfd38b2dd6708f8b1e20a
Ivan <ipkozyrin@gmail.com>
2026-08-18 19:00:51 -0300

    update

--- test/helpers/mocks.ts
+++ test/helpers/mocks.ts
@@ -5,6 +5,7 @@
  * All mocks follow the DRY principle and provide type-safe implementations.
  */
 
+import { jest } from '@jest/globals';
 import type { PinoLogger } from 'nestjs-pino';
 import type { ConfigService } from '@nestjs/config';
 
